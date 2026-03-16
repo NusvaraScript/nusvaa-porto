d6e144ba312add7d9bacec3ff33d72281d162ce9
Nusvara <yusufusman11111111@gmail.com>
2026-03-16 16:02:40 +0700

    about page done for now

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -1,11 +1,13 @@
 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 items-stretch border-t border-white/10">
-    <x-section section="contact" title="Wanna know more? Contact me!"
-        description="I'm open to freelance projects, collaborations, or just chatting about anything I know of."
-        :border="false">
-    </x-section>
-    <div class="border-l border-white/10">
-        <p></p>
-        <div class="p-8 flex flex-col gap-4">
+    <div class="pt-8 md:py-8 px-6">
+        <p class="text-xs md:text-sm text-blue-500 uppercase tracking-widest">
+            > contact</p>
+        <h1 class="text-2xl md:text-3xl font-bold mt-2">Wanna know more? Contact me!</h1>
+        <p class="text-sm my-4 text-gray-300 leading-relaxed">I'm open to freelance projects, collaborations, or just
+            chatting about anything I know of.</p>
+    </div>
+    <div class="border-white/10">
+        <div class="pb-8 md:py-8 px-6 flex flex-col gap-4">
             <x-button route="https://github.com/NusvaraScript" variant="outline" target="_blank"
                 class="w-full flex justify-between">
                 <span class="text-blue-500">[Github]</span>
